f0884ea0f04aa78a939f66d29513e7f8a2f88efc
Baku <millet_c@epitech.net>
2010-12-21 18:52:05 +0100

    Uml complet + server

---
+++ server/Room.h
@@ -0,0 +1,37 @@
+/*
+** Room.h
+** Login : <baku@anthrax>
+** Started on  Tue Dec 21 17:59:16 2010 baku
+** $Id$
+**
+** Author(s):
+**  - baku <millet_c@epitech.net>
+**
+** Copyright (C) 2010 baku
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef   	ROOM_H_
+# define   	ROOM_H_
+
+class   Room : public Server
+{
+public:
+
+private:
+  int   limit_player;
+};
+
+#endif 	    /* !ROOM_H_ */
---
+++ server/Server.h
@@ -0,0 +1,40 @@
+/*
+** manager.h
+** Login : <baku@anthrax>
+** Started on  Tue Dec 21 17:44:00 2010 baku
+** $Id$
+**
+** Author(s):
+**  - baku <millet_c@epitech.net>
+**
+** Copyright (C) 2010 baku
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef   	MANAGER_H_
+# define   	MANAGER_H_
+
+
+class   Imanager
+{
+ public:
+ void   send();
+ void   reveive();
+ void   accept();
+ Room   room;
+private:
+};
+
+#endif 	    /* !MANAGER_H_ */
